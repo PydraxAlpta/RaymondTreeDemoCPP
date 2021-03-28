d69f38de9c62137d70e50a6421735ae2ba033279
PydraxAlpta <49099987+PydraxAlpta@users.noreply.github.com>
2021-03-28 12:15:18 +0530

    added comments and license

--- Code/main.cpp
+++ Code/main.cpp
@@ -1,6 +1,26 @@
+/*
+    Raymond's Algorithm is a lock based algorithm for mutual exclusion on a distributed system. 
+    This is a C++ implementation intended to demonstrate how it works and is intended for educational purposes only.
+    Copyright (C) 2021 Pauras Ranade (PydraxAlpta)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include <iostream>
 #include <vector>
 
+// this enum is used to implement the "temporary reversal of link direction" that the basic tree structure cannot incorporate
 enum TokenPolarity
 {
     LEFT,
